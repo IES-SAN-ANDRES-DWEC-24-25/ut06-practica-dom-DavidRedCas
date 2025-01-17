8ca7f1ffbb28634e8d651b46588575e4e64e7105
David Redondo <81869731+DavidRedCas@users.noreply.github.com>
2025-01-17 19:10:02 +0100

    acabar 1 y 3

--- js/ejercicio01.js
+++ js/ejercicio01.js
@@ -1,3 +1,7 @@
 document.getElementById("boton").addEventListener("click", () => {
     document.getElementById("encabezado").style.color = document.getElementById("color").value;
+    /*
+    let h1 = document.getElementsByTagName("h1");
+    h1.style.color = document.getElementById("color").value;
+    */
 });
---
+++ js/ejercicio03.js
@@ -0,0 +1,3 @@
+document.getElementByTagName("button").addEventListener("click", () => {
+    
+});
