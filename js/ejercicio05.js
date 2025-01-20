c4b70747b928ee722f07947fa1b4f2d08a23b247
David Redondo <81869731+DavidRedCas@users.noreply.github.com>
2025-01-20 15:49:30 +0100

    ej5 acabado

--- js/ejercicio05.js
+++ js/ejercicio05.js
@@ -2,19 +2,16 @@ const headers = document.querySelectorAll("h3");
 
 headers.forEach((header) => {
     header.addEventListener("click", (event) => {
-        const targetH3 = event.target;
-        const grado = targetH3.textContent.trim();
-        const contenedor = targetH3.parentElement;
+        const h3 = event.target;
+        const grado = h3.textContent.trim();
+        const contenedor = h3.parentElement;
         const turno = contenedor.id;
-        const lista = targetH3.nextElementSibling;
+        const lista = h3.nextElementSibling;
         const grupos = Array.from(lista.children).map((li) => li.textContent);
 
-        const mensaje = `
-            Has elegido ${grado} que se imparte en turno de ${turno}.
-            Los nº de grupos que se imparten son ${grupos.length}: ${grupos.join(", ")}.
-        `;
+        const mensaje = `Has elegido ${grado} que se imparte en turno de ${turno}.\nLos nº de grupos que se imparten son ${grupos.length}: ${grupos.join(", ")}.`;
 
-        const resultado = document.createElement("p");
+        let resultado = document.createElement("p");
         resultado.textContent = mensaje;
         document.body.appendChild(resultado);
     });
